076710241113ae3f83b5a5070df538af527ce162
Sergei Kahnwald <vvintpann@gmail.com>
2025-03-08 17:42:29 +0300

    init

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import { StoryBox } from 'storybox-react';
+import 'storybox-react/dist/styles.css';
+import { stories } from './stories.ts';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <StoryBox stories={stories} />,
+);
---
+++ src/stories.ts
@@ -0,0 +1,3 @@
+import { FC } from 'react';
+
+export const stories: Record<string, FC> = { Starter: () => null };
